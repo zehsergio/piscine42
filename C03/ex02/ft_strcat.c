0085991b84c763d37a3cb72722ae0dd21375e22a
zehsergio <jsdiasjr@gmail.com>
2025-07-03 00:12:44 +0200

    piscine 42 2025

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdias-ju <jdias-ju@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/02 23:16:27 by jdias-ju          #+#    #+#             */
+/*   Updated: 2025/07/03 00:12:06 by jdias-ju         ###   ########.ch       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	// Avança até o final de dest
+	while (dest[i])
+		i++;
+
+	// Copia src para dest a partir do final
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+// ANOTHER OPTION WITH POINTERS
+/*
+char	*ft_strcat(char *dest, char *src)
+{
+	char *start = dest;
+	
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*src)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return (start);
+}
+*/
+
+/*
+int	main(void)
+{
+	char dest[] = "Hello ";
+	char src[] = "World!";
+	printf("%s", ft_strcat(dest, src));
+}
+*/
