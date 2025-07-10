4eaf6dd1b25e9007392e0f0a1d29adf8c2bb59bf
zehsergio <jsdiasjr@gmail.com>
2025-07-11 00:17:01 +0200

    examtest

--- exam-tests/ft_print_params/ft_print_params.c
+++ exam-tests/ft_print_params/ft_print_params.c
@@ -5,6 +5,16 @@ int main(int argc, char **argv)
 	int i;
 	int j;
 
-	i = 0;
-	while (i > argc)
+	j = 1;
+	while (j < argc)
+	{
+		i = 0;
+		while (argv[j][i])
+		{
+			write (1, &argv[j][i], 1);
+			i++;
+		}
+		write (1, "\n", 1);
+		j++;
+	}
 }
